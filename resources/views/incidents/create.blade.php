ef85a5a902fd1124c2ab193da7f7c49abce81b1a
Moid <moidkhan22@yahoo.com>
2024-02-19 01:11:04 +0500

    done

--- resources/views/incidents/create.blade.php
+++ resources/views/incidents/create.blade.php
@@ -51,13 +51,14 @@
                 <div class="card form-holder">
                     <div class="card-body">
                         @if (Session::has('success'))
-                        <div class="alert alert-success" role="alert">
-                            {{ Session::get('success') }}
-                        </div>
+                            <div class="alert alert-success" role="alert">
+                                {{ Session::get('success') }}
+                            </div>
                         @endif
                         <div class="w-100 text-center">
-                            <img class="img-fluid " src="{{ env('DO_CDN_ENDPOINT')."/{$kit->preventionAdvisor->company->logo}" }}"
-                            alt="">
+                            <img class="img-fluid "
+                                src="{{ env('DO_CDN_ENDPOINT') . "/{$kit->preventionAdvisor->company->logo}" }}"
+                                alt="">
                         </div>
                         <h3 class="text-center mt-3">{{ $kit->preventionAdvisor->company->name }}</h3>
                         <p class="text-center text-grey">Please enter the incident details</p>
@@ -71,13 +72,44 @@
                                 <input type="text" name="employee_name" class="form-control" placeholder="Name"
                                     required />
                             </div>
-                            @if(!empty($questions))
+                            @if (!empty($questions))
                                 @foreach ($questions as $question)
-                                    <div class="form-group">
+                                    {{-- <div class="form-group">
                                         <label>{{ $question->question }}</label>
                                         <textarea rows="2" class="form-control" name="question_{{ $question->id }}" id=""
                                             cols="30" rows="10" required></textarea>
-                                    </div>
+                                    </div> --}}
+
+                                    <?php
+                                    $contentArray = json_decode($question->content, true);
+                                    if ($contentArray && is_array($contentArray) && !empty($contentArray)) {
+                                        $type = $contentArray[0]['type'];
+                                        $label = $contentArray[0]['label'];
+                                        switch ($type) {
+                                            case 'select':
+                                                $fieldValue = $contentArray[0]['values'];
+                                                echo '<div class="form-group">
+                                                    <label>'.__('Company Name').'</label>
+                                                    <select name="question_'.$question->id.'" class="form-control">';
+                                                        foreach ($fieldValue as $fV) {
+                                                            echo '<option value="'.$fV['value'].'">'. $fV['label'].'</option>';
+                                                        }
+                                                echo '</select>
+                                                </div>';
+                                                break;
+
+                                            case 'text'
+
+                                            break;
+
+                                            
+                                            default:
+                                                # code...
+                                                break;
+                                        }
+                                      
+                                    }
+                                    ?>
                                 @endforeach
                             @endif
 
@@ -132,6 +164,7 @@
 
     let attempts = 0;
     let maxAttempts = 3;
+
     function blockAccess() {
         console.log('Maximum attempts reached. Blocking access.');
         // Redirect to a blocked page or show an error message
@@ -140,9 +173,8 @@
 
     function authenticate() {
         var enteredPassword = window.prompt('Please enter the password:');
-        
-        if (enteredPassword !== null && enteredPassword === password) {
-        } else {
+
+        if (enteredPassword !== null && enteredPassword === password) {} else {
             // Increment the attempts
             attempts++;
 
@@ -156,9 +188,9 @@
         }
     }
 
-    if(password != null){
+    if (password != null) {
         var enteredPassword = window.prompt('Please enter the password:');
-            authenticate();
+        authenticate();
     }
 </script>
 
