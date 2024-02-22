633b70fc0facac4d870c95aea98e60aaf2560d74
Moid Khan <mueedkhan766@gmail.com>
2024-02-23 01:44:37 +0500

    done

--- app/Http/Controllers/KitsController.php
+++ app/Http/Controllers/KitsController.php
@@ -61,7 +61,7 @@ class KitsController extends Controller
 
     Storage::disk('do')->put(
         "{$folder}/{$fileName}",
-        (QrCode::format('png')->size(200)->merge(public_path('logo.png'), 0.8, true)->generate($absoluteUrl)),
+        (QrCode::format('png')->size(200)->merge(public_path('logo.png'), 0.8, true)->errorCorrection('H')->generate($absoluteUrl)),
         'public'
     );
 
