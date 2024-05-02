4dd22b5a62175b15e809259de0d683940526cb6f
Moid <moidkhan22@yahoo.com>
2024-05-02 14:36:43 +0500

    done

--- app/Http/Controllers/KitsController.php
+++ app/Http/Controllers/KitsController.php
@@ -79,7 +79,6 @@ class KitsController extends Controller
             // Generate the QR code
             $qrCode = QrCode::format('png')
                 ->size(400)
-                ->merge(public_path('logo.jpg'), 0.8, true)
                 ->errorCorrection('H')
                 ->generate($absoluteUrl);
 
