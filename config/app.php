1df08c5c7f9b620793c1258a3e354eb25e540dbc
TMS <moidzaeem@gmail.com>
2024-05-16 03:32:13 +0500

    done

--- config/app.php
+++ config/app.php
@@ -186,7 +186,6 @@ return [
         Barryvdh\DomPDF\ServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
          Collective\Html\HtmlServiceProvider::class,
-         'Intervention\Image\ImageServiceProvider'
 
     ])->toArray(),
 
@@ -208,7 +207,7 @@ return [
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'Form' => Collective\Html\FormFacade::class,
     'Html' => Collective\Html\HtmlFacade::class,
-    'Image' => 'Intervention\Image\Facades\Image'
+
 
     ])->toArray(),
 
