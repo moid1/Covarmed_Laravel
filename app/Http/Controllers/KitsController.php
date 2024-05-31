536dc9c26f8b61c4d2bfd9554c45ca615a1fa80b
TMS <moidzaeem@gmail.com>
2024-05-31 17:18:38 +0500

    done

--- app/Http/Controllers/KitsController.php
+++ app/Http/Controllers/KitsController.php
@@ -90,7 +90,7 @@ class KitsController extends Controller
 
             $textImage = Image::canvas(200, 30, '#FFFFFF');
             $textImage->text($request->name, 120, 15, function ($font) {
-                $font->size(40);
+                $font->size(140);
                 $font->align('center');
                 $font->valign('middle');
             });
