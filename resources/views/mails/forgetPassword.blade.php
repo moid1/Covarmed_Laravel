9d2fb3278f66e6f1ebae4cbbd2e326716544628e
Faruk <faruk@Faruk-Air.bbrouter>
2025-11-02 16:27:34 +0300

    Destructive deletion of all files

--- resources/views/mails/forgetPassword.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Forgot Password</h1>
-<p>Hello!</p>
-<p>We received a request to reset your password. You can use the follow link to create your new password: <a href="{{ route('reset.password.get', $token) }}">Reset Password</a> </p>
-<p>Did you not ask for this request? Please contact info@covarmed.be. </p>
-<p>Kind regards</p>
-<p>Covarmed</p>
-<img height="50" src="https://covarmed.sfo3.cdn.digitaloceanspaces.com/logo.png" alt="" srcset="">
